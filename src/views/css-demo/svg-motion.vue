91dbeb1423f0da679ffba7264e6f8a3ad3b37ac8
ihuangxiaomin <1219549841@qq.com>
2024-06-27 18:23:41 +0800

    31232131

--- src/views/css-demo/svg-motion.vue
+++ src/views/css-demo/svg-motion.vue
@@ -1,5 +1,95 @@
 <template>
   <div class="warpper">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      width="300px"
+      height="200px"
+      viewBox="0 0 685 310"
+    >
+      <defs>
+        <path
+          id="loop-normal"
+          d="M10 80 Q 77.5 10, 145 80 T 280 80"
+        />
+
+        <!-- radial gradient has same color at constnnt distance from center,
+         regardless of direction-->
+        <radialGradient
+          id="fade"
+          cx="0"
+          cy="0"
+          fx="0"
+          fy="0"
+          r="200"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#000" stop-opacity="1" offset="0" />
+          <!-- change offset to make faiding "tail" longer or shorter -->
+          <stop stop-color="#000" stop-opacity="1" offset="0.6" />
+          <stop stop-color="#000" stop-opacity="0" offset="1" />
+        </radialGradient>
+
+        <!-- mask shows only stroked area of path. Stroke length must be longer
+         than gradient radius, as path can be curved-->
+        <mask id="tail" maskUnits="userSpaceOnUse">
+          <use
+            style="fill: none; stroke: #FF8A00; stroke-width: 6; stroke-dasharray: 250 1664"
+            xlink:href="#loop-normal"
+          >
+            <animate
+              attributeName="stroke-dashoffset"
+              from="250"
+              to="-1664"
+              dur="3s"
+              repeatCount="indefinite"
+            />
+          </use>
+        </mask>
+      </defs>
+      <use style="fill: none; stroke: #ccc; stroke-width: 6" xlink:href="#loop-normal" />
+
+      <!-- outer group makes sure mask is applied after animateMotion -->
+      <g style="mask: url(#tail)">
+        <!-- circle with gradient radius -->
+        <circle style="fill: url(#fade)" cx="0" cy="0" r="200">
+          <animateMotion keyPoints="0;1" keyTimes="0;1" dur="3s" repeatCount="indefinite">
+            <mpath xlink:href="#loop-normal" />
+          </animateMotion>
+        </circle>
+      </g>
+
+      <!-- leading dot circle -->
+      <circle style="fill: #000" cx="0" cy="0" r="7">
+        <animateMotion keyPoints="0;1" keyTimes="0;1" dur="3s" repeatCount="indefinite">
+          <mpath xlink:href="#loop-normal" />
+        </animateMotion>
+      </circle>
+    </svg>
+    
+
+    <div class="svg-motion-0">
+      <svg width="56px" height="6px" viewBox="-2 -2 56 6" fill="none">
+        <path
+          d="M1.43433 1.28612L51.0056 1.28612"
+          stroke="#ebf2f9"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="shadow"
+        />
+        <path
+          fill="transparent"
+          stroke="#FF8A00"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M1.43433 1.28612L51.0056 1.28612"
+          class="line1"
+        ></path>
+      </svg>
+    </div>
+
     <div class="svg-motion-1">
       <svg width="300px" height="175px" version="1.1">
         <path
@@ -66,6 +156,36 @@
 <script lang="ts" setup></script>
 
 <style lang="less" scoped>
+  .svg-motion-0 {
+    background: #fff;
+    height: 300px;
+    width: 100%;
+    margin-top: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    svg {
+      width: 600px;
+      height: 200px;
+      display: block;
+      position: absolute;
+    }
+    svg .line1 {
+      stroke-dasharray: 55, 104;
+      stroke-dashoffset: 50;
+      animation: dash 2s linear infinite;
+      filter: drop-shadow(0 0 2px #17a6f7) drop-shadow(0 0 2px #2aacf5);
+    }
+    @keyframes dash {
+      from {
+        stroke-dashoffset: 159;
+      }
+      to {
+        stroke-dashoffset: 0;
+      }
+    }
+  }
+
   .shadow {
     -webkit-filter: drop-shadow(0px 0px 2px #ebf2f97a);
     filter: drop-shadow(0px 0px 2px #ebf2f97a);
--- src/views/css-demo/wegame.vue
+++ src/views/css-demo/wegame.vue
@@ -1,14 +1,61 @@
 <template>
-  <div class="warpper">
-    <div class="g-scroll" id="g-scroll"></div>
-    <div class="g-wrap">
-      <!-- <div class="g-bg"></div>
-      <div class="g-container">
-        <div class="g-wegame"></div>
-        <div class="g-mask"></div>
-        <div class="g-logo"></div>
-      </div> -->
-    </div>
+  <div class="warpper w-full h-full flex justify-center items-center">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      width="222"
+      height="39"
+      viewBox="0 0 222 39"
+    >
+      <defs>
+        <path id="loop-normal" d="M3.67773 38.5709V3.125H221.778" />
+
+        <!-- radial gradient has same color at constnnt distance from center,
+         regardless of direction-->
+        <radialGradient
+          id="fade"
+          cx="0"
+          cy="0"
+          fx="0"
+          fy="0"
+          r="100"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#01f061" stop-opacity="1" offset="0" />
+          <!-- change offset to make faiding "tail" longer or shorter -->
+          <stop stop-color="#01f061" stop-opacity="1" offset="0.6" />
+          <stop stop-color="#01f061" stop-opacity="0" offset="1" />
+        </radialGradient>
+
+        <!-- mask shows only stroked area of path. Stroke length must be longer
+         than gradient radius, as path can be curved-->
+        <mask id="tail" maskUnits="userSpaceOnUse">
+          <use
+            style="fill: none; stroke: #ff8a00; stroke-width: 6; stroke-dasharray: 250 1664"
+            xlink:href="#loop-normal"
+          >
+            <animate
+              attributeName="stroke-dashoffset"
+              from="250"
+              to="-1664"
+              dur="3s"
+              repeatCount="indefinite"
+            />
+          </use>
+        </mask>
+      </defs>
+      <use style="fill: none; stroke: #ccc; stroke-width: 6" xlink:href="#loop-normal" />
+
+      <!-- outer group makes sure mask is applied after animateMotion -->
+      <g style="mask: url(#tail)">
+        <!-- circle with gradient radius -->
+        <circle style="fill: url(#fade)" cx="0" cy="0" r="100">
+          <animateMotion keyPoints="0;1" keyTimes="0;1" dur="3s" repeatCount="indefinite">
+            <mpath xlink:href="#loop-normal" />
+          </animateMotion>
+        </circle>
+      </g>
+    </svg>
   </div>
 </template>
 <script lang="ts" setup></script>
@@ -16,111 +63,5 @@
 <style lang="less" scoped>
   .warpper {
     position: relative;
-  }
-  .g-scroll {
-    position: relative;
-    height: 500vh;
-  }
-
-  .g-wrap {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    overflow: hidden;
-  }
-
-  .g-container {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    animation-name: scale;
-    animation-duration: 10s;
-    animation-timeline: box-move;
-  }
-
-  .g-bg {
-    position: fixed;
-    width: 100vw;
-    height: 100vh;
-    // background: repeating-linear-gradient(45deg, #000, #000 10px, #fff 10px, #fff 20px);
-    background: url(@/assets/images/sc02.png);
-    background-size: 80%;
-    background-position: center;
-    background-repeat: no-repeat;
-  }
-
-  .g-wegame {
-    position: absolute;
-    width: 100vw;
-    height: 100vh;
-    background: linear-gradient(-75deg, #715633, #2b2522);
-    mask: url(@/assets/svg/we.svg), linear-gradient(#fff, #fff);
-    mask-repeat: no-repeat;
-    mask-position: center center;
-    -webkit-mask-composite: xor;
-  }
-
-  .g-mask {
-    position: aboslute;
-    width: 100vw;
-    height: 100vh;
-    background: linear-gradient(-75deg, #715633, #2b2522);
-    z-index: 2;
-    animation-name: reOpacityChange;
-    animation-duration: 10s;
-    animation-timeline: box-move;
-    animation-function-timing: linear;
-  }
-
-  .g-logo {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(calc(-50% - 2px), calc(-50% - 2px));
-    width: 448px;
-    height: 532px;
-    background: url(@/assets/svg/we.svg);
-    background-repeat: no-repeat;
-    background-position: center center;
-    z-index: 3;
-    animation-name: reOpacityChange;
-    animation-duration: 10s;
-    animation-timeline: box-move;
-  }
-
-  /* @scroll-timeline box-move {
-    source: selector('#g-scroll');
-    orientation: 'vertical';
-  } */
-  @keyframes reOpacityChange {
-    0%,
-    50% {
-      opacity: 1;
-    }
-    90% {
-      opacity: 0.2;
-    }
-    100% {
-      opacity: 0;
-    }
-  }
-
-  @keyframes scale {
-    0% {
-      transform: scale(1);
-    }
-    40% {
-      transform: scale(5);
-    }
-    70% {
-      transform: scale(20);
-    }
-    100% {
-      transform: scale(60);
-    }
   }
 </style>
