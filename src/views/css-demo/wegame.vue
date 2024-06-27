ecdd78034d9fb1cf321724f191e0a10d101ca0ad
ihuangxiaomin <1219549841@qq.com>
2024-06-27 18:45:02 +0800

    123321

--- src/views/css-demo/wegame.vue
+++ src/views/css-demo/wegame.vue
@@ -56,6 +56,86 @@
         </circle>
       </g>
     </svg>
+    <!-- down-flow -->
+    <svg width="222" height="39" viewBox="0 0 222 39" fill="none">
+      <path
+        d="M3.67773 38.5709V3.125H221.778"
+        stroke="#ebf2f9"
+        stroke-width="6"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="up-shadow"
+      />
+      <path
+        fill="transparent"
+        stroke="#15eb26"
+        stroke-width="6"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3.67773 38.5709V3.125H221.778"
+        class="up-line1"
+      ></path>
+    </svg>
+    <!-- input-flow -->
+    <svg width="116" height="7" viewBox="0 0 116 7" fill="none">
+      <path
+        d="M0.921875 3.85156L115.499 3.85157"
+        stroke="#ebf2f9"
+        stroke-width="6"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="up-shadow"
+      />
+      <path
+        fill="transparent"
+        stroke="#15eb26"
+        stroke-width="6"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M0.921875 3.85156L115.499 3.85157"
+        class="up-line1"
+      ></path>
+    </svg>
+    <!-- output-flow -->
+    <svg width="140" height="65" viewBox="0 0 140 65" fill="none">
+      <path
+        d="M0.265625 3.125H31.3375C41.2786 3.125 49.3375 11.1839 49.3375 21.125V43.8398C49.3375 53.781 57.3963 61.8398 67.3375 61.8398H139.378"
+        stroke="#ebf2f9"
+        stroke-width="6"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="up-shadow"
+      />
+      <path
+        fill="transparent"
+        stroke="#15eb26"
+        stroke-width="6"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M0.265625 3.125H31.3375C41.2786 3.125 49.3375 11.1839 49.3375 21.125V43.8398C49.3375 53.781 57.3963 61.8398 67.3375 61.8398H139.378"
+        class="up-line1"
+      ></path>
+    </svg>
+    <!-- up-flow -->
+    <svg width="222" height="39" viewBox="0 0 222 39" fill="none">
+      <path
+        d="M3.67773 0.405681V35.8516H221.778"
+        stroke="#ebf2f9"
+        stroke-width="6"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="up-shadow"
+      />
+      <path
+        fill="transparent"
+        stroke="#15eb26"
+        stroke-width="6"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3.67773 0.405681V35.8516H221.778"
+        class="up-line1"
+      ></path>
+    </svg>
   </div>
 </template>
 <script lang="ts" setup></script>
@@ -63,5 +143,23 @@
 <style lang="less" scoped>
   .warpper {
     position: relative;
+  }
+
+  .up-shadow {
+    -webkit-filter: drop-shadow(0px 0px 2px #ebf2f97a);
+    filter: drop-shadow(0px 0px 2px #ebf2f97a);
+  }
+  svg .up-line1 {
+    stroke-dasharray: 100, 104;
+    animation: dash 2s linear infinite;
+    //filter: drop-shadow(0 0 2px #17a6f7) drop-shadow(0 0 2px #2aacf5);
+  }
+  @keyframes dash {
+    from {
+      stroke-dashoffset: 204;
+    }
+    to {
+      stroke-dashoffset: 0;
+    }
   }
 </style>
